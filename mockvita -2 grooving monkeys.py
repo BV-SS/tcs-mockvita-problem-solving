e1fe696d21a94f1218a6f96304b0a90fa3befc1c
bharti <65727171+BV-SS@users.noreply.github.com>
2020-08-14 16:05:23 +0530

    Update mockvita -2 grooving monkeys.py

--- mockvita -2 grooving monkeys.py
+++ mockvita -2 grooving monkeys.py
@@ -1,29 +1,31 @@
-arr=list(map(int,input().split()))
-
-l=len(arr)
-
-arr.insert(0,0)
-arr2=list(range(0,l+1))
-
-count=0
-
-orig=arr2
-while True:
-    arr3=[0]*(l+1)
-    for i in range(1,l+1):
-        arr3[arr[i]]=arr2[i]
-    count+=1
+n=int(input())
+arr=[]
+for j in range(n):
+    num=int(input())
+    arr=list(map(int,input().split()))
+    arr.insert(0,0)
     
-    if arr3==orig:
-        print(count)
-        break
-    
-    
-    else:
-        arr2=arr3
-    
-  
+    arr2=list(range(0,num+1))
 
-    
+    count=0
+
+    orig=arr2
+    while True:
+        arr3=[0]*(num+1)
+        for i in range(1,num+1):
+            arr3[arr[i]]=arr2[i]
+        count+=1
+
+        if arr3==orig:
+            print(count)
+            break
+
+
+        else:
+            arr2=arr3
+
+
+
+
 
     
